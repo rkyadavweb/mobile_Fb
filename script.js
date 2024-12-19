50dbc9f00c743214d9116a334b88914a2d2d99cb
rkyadavweb <rkyadavbya@gmail.com>
2024-12-19 13:04:30 +0530

    Uploaded on 19-12-2024

---
+++ script.js
@@ -0,0 +1,21 @@
+var settingsToggle = document.querySelector(".setting-toggle");
+function settingToggle(){
+    settingsToggle.classList.toggle("setting-toggle-height");
+};
+var helpstoogle = document.querySelector(".help-toggle")
+function helpToggle(){
+    helpstoogle.classList.toggle("help-toggle-height");
+}
+var also = document.querySelector(".also-toggle");
+function AlsoToggle(){
+    also.classList.toggle("help-toggle-height");
+}
+var creator = document.querySelector(".creator-toggle");
+function creatortoggle(){
+    creator.classList.toggle("help-toggle-height")
+}
+let a = document.getElementsByClassName("post-name-top");
+console.log(a);
+console.log(a.length);
+let footer = document.getElementsByClassName("footer");
+console.log(footer);
